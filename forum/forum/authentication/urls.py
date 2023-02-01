e600bda3e40052d252c7b92cb6fbc7dea164776c
Yehor Chervony <chervony.egor@gmail.com>
2023-02-02 00:11:56 +0200

    Added logout route

--- forum/forum/authentication/urls.py
+++ forum/forum/authentication/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.login_user, name="login"),
     path('signup/', views.signup_user, name="signup"),
+    path('logout/', views.logout_user, name="logout"),
 ]

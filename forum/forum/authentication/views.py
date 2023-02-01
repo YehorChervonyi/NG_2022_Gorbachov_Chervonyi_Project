b848e747fd638682f7fb1e5294c5f563c8083d9b
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-01 15:30:55 +0200

    Added function log in which redirect to home page

--- forum/forum/authentication/views.py
+++ forum/forum/authentication/views.py
@@ -1,14 +1,35 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'authentication/main.html')
-def login(request):
-    return render(request, 'authentication/login.html')
-def signup(request):
+
+
+# This function logging user to his account.
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, "Username or Password doesnt exist, try again...")
+            return redirect('login')
+    context = {}
+    return render(request, 'authentication/login.html', context)
+
+
+# This function will registrate user and create his account, and redirect it on home page.
+def signup_user(request):
     form = UserCreationForm()
+
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():

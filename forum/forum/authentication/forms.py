002b3034d9fe26ce36fcffaf7c9c23b47ff74947
Yehor Chervony <chervony.egor@gmail.com>
2023-02-02 00:11:06 +0200

    Created custom form for registration + input fields html class style

--- forum/forum/authentication/forms.py
+++ forum/forum/authentication/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class RegisterUserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50)
-    second_name = forms.CharField(max_length=50)
-    email = forms.EmailField()
+    class Meta:
+        model= User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'})) 
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'

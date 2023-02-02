dbabd09f39525d09e464910e37a9a255693dde69
Yehor Chervony <chervony.egor@gmail.com>
2023-02-02 19:17:31 +0200

    Changed placeholder, help_text for form fields

--- forum/forum/authentication/forms.py
+++ forum/forum/authentication/forms.py
@@ -7,13 +7,18 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model= User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'firstname'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'lastname'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder':'email'}))
 
     def __init__(self, *args, **kwargs):
         super(RegisterUserForm, self).__init__(*args, **kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].help_text="Username required. 50 characters or fewer. Letters, digits and @/./+/-/_ only."
         self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password1'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Password2'
+        self.fields['password2'].help_text = ''

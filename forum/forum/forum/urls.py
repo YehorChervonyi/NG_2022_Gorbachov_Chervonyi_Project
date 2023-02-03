d0c97fbe6f5cedc3c39cb92fc167754e0d2d5dd7
Yehor Chervony <chervony.egor@gmail.com>
2023-02-03 23:53:27 +0200

    Added static name for /admin

--- forum/forum/forum/urls.py
+++ forum/forum/forum/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('authentication.urls'))
 ]

a8c002361a7a4a35f0690f34217a97e23462e751
Yehor Chervony <chervony.egor@gmail.com>
2023-02-03 23:52:23 +0200

    Added create disussion, themes function | Updating themes in navbar | Render discuss page

--- forum/forum/authentication/views.py
+++ forum/forum/authentication/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from . forms import RegisterUserForm, User, ThemeForm, DiscussionForm
+from . models import Theme, Discussion
+
+
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'authentication/main.html')
+    themes = Theme.objects.all()
+    discussions=Discussion.objects.all()
+    context={
+        'discussions':discussions,
+        'themes':themes
+    }
+    return render(request, 'authentication/main.html', context)
 
 # This function logging user to his account.
 def login_user(request):
+    themes = Theme.objects.all()
     if request.method == "POST":
         email = request.POST['email'].strip()
         password = request.POST['password'].strip()
@@ -34,12 +44,15 @@ def login_user(request):
             else:
                 messages.warning(request, "Email or Password doesnt exist, try again...")
 
-    context = {}
+    context = {
+        'themes':themes
+    }
     return render(request, 'authentication/login.html', context)
 
 
 # This function will registrate user and create his account, and redirect it on home page.
 def signup_user(request):
+    themes = Theme.objects.all()
     form = RegisterUserForm()
 
     if request.method == "POST":
@@ -51,21 +64,52 @@ def signup_user(request):
             messages.success(request, "Registration successful")
             return redirect('login')
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'themes':themes}
     return render(request, 'authentication/signup.html', context)
 
 
 def logout_user(request):
+    themes = Theme.objects.all()
     logout(request)
     messages.info(request, "You are logged out")
     return redirect('home')
 
 def create_theme(request):
+    themes = Theme.objects.all()
+    
     form = ThemeForm
-    context = {'form': form}
+    if request.method == "POST":
+        form = ThemeForm(request.POST)
+        if form.is_valid():
+            theme = form.save(commit=False)
+            theme.save()
+            messages.success(request, "Theme created")
+            return redirect('home')
+        themes = Theme.objects.all()
+    context = {'form': form,
+        'themes':themes}
     return render(request, 'authentication/createtheme.html', context)
 
 def create_discussion(request):
+    themes = Theme.objects.all()
     form = DiscussionForm
-    context = {'form': form}
+    if request.method == "POST":
+        form = DiscussionForm(request.POST)
+        if form.is_valid():
+            discussion = form.save(commit=False)
+            discussion.save()
+            messages.success(request, "Discussion created. We believe you wil enjoy")
+            return redirect('home')
+    context = {'form': form,
+                'themes':themes}
     return render(request, 'authentication/creatediscussion.html', context)
+
+def discusspage(request, page):
+    themes = Theme.objects.all()
+    context={
+        'page':page,
+        'themes':themes,
+    }
+    return render (request, 'authentication/discussion.html', context)

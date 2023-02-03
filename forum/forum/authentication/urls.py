c81e6d73a1b96079f8649f3693d268eb61b67aaa
Yehor Chervony <chervony.egor@gmail.com>
2023-02-03 21:33:31 +0200

    Created route for create descussion

--- forum/forum/authentication/urls.py
+++ forum/forum/authentication/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('signup/', views.signup_user, name="signup"),
     path('logout/', views.logout_user, name="logout"),
     path('createtheme/', views.create_theme, name="createtheme"),
+    path('creatediscussion/', views.create_discussion, name="creatediscussion"),
 ]

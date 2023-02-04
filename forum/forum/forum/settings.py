5f192ef38d85005ae29bbb28548268505da57a66
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-04 13:38:25 +0200

    Added basedir static

--- forum/forum/forum/settings.py
+++ forum/forum/forum/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+     BASE_DIR / 'static'
+]
 
 
 # Default primary key field type

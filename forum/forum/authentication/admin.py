dd0bb7c8f749d21362507276f6cdd6693298c571
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-04 13:31:42 +0200

    Registred comments to admin panel

--- forum/forum/authentication/admin.py
+++ forum/forum/authentication/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Discussion, Theme
+from .models import Discussion, Theme, Comments
 
 
 admin.site.register(Discussion)
 admin.site.register(Theme)
+admin.site.register(Comments)
 

27d2439efa96e132e35cb166ccf7a678962487d3
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-04 13:33:09 +0200

    Added model for comments for database

--- forum/forum/authentication/models.py
+++ forum/forum/authentication/models.py
@@ -1,10 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
 User._meta.get_field('email')._unique = True
-
-
 
 
 class Theme(models.Model):
@@ -20,6 +19,18 @@ class Discussion(models.Model):
     theme = models.ForeignKey(Theme, null=False, on_delete=models.CASCADE)
     update_time = models.DateTimeField(auto_now=True)
     create_time = models.DateTimeField(auto_now_add=True)
+    # author_discussion = models.ForeignKey(get_user_model(), null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name_discussion
+
+
+class Comments(models.Model):
+    text_comment = models.TextField(max_length=1024)
+    discussion = models.ForeignKey(Discussion, null=False, on_delete=models.CASCADE)
+    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    # author_comment = models.ForeignKey(get_user_model(), null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.text_comment

bdd434e7340e3eb746c5b6aecbee60688855760b
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-04 13:39:34 +0200

    Trying to add favicon(Not working)

--- forum/forum/forum/urls.py
+++ forum/forum/forum/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', include('authentication.urls'))
+    path('', include('authentication.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
+
 ]

7ce8ffc9df943de925622c8e59b2c4dd0c896007
Yehor Chervony <chervony.egor@gmail.com>
2023-02-07 18:39:04 +0200

    Static root for media directory

--- forum/forum/authentication/urls.py
+++ forum/forum/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home_page, name="home"),
     path('login/', views.login_user, name="login"),
@@ -11,4 +12,4 @@ urlpatterns = [
     path('creatediscussion/', views.create_discussion, name="creatediscussion"),
     path('<str:page>/', views.themepage),
     path('<str:page>/<str:discussionid>/', views.discussionpage)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

8e62a44d97638aa24b3d2a42c2c3c8015c083161
Yehor Chervony <chervony.egor@gmail.com>
2023-02-08 16:41:18 +0200

    Added notification page url

--- forum/forum/authentication/urls.py
+++ forum/forum/authentication/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('logout/', views.logout_user, name="logout"),
     path('createtheme/', views.create_theme, name="createtheme"),
     path('creatediscussion/', views.create_discussion, name="creatediscussion"),
+    path('notifications/', views.notificationspage, name="notifications"),
     path('<str:page>/', views.themepage),
     path('<str:page>/<str:discussionid>/', views.discussionpage)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

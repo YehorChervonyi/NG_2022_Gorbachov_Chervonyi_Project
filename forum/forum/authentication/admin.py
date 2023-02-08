ffc7d2349f0f619c0367832eab8afd6efcd98368
Evgeniy Gorbachov <zekagor@gmail.com>
2023-02-08 13:51:12 +0200

    For admin added notification settings

--- forum/forum/authentication/admin.py
+++ forum/forum/authentication/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Discussion, Theme, Comments
+from .models import Discussion, Theme, Comments, Notification
 
 
 admin.site.register(Discussion)
 admin.site.register(Theme)
 admin.site.register(Comments)
+admin.site.register(Notification)
 

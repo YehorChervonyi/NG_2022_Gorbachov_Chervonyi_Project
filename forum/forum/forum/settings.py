5cafa6cf2dce36a8f530188da5ed9c6d66c5881a
Yehor Chervony <chervony.egor@gmail.com>
2023-02-16 14:39:31 +0200

    Added email credentials settings

--- forum/forum/forum/settings.py
+++ forum/forum/forum/settings.py
@@ -132,4 +132,13 @@ STATICFILES_DIRS =[
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'youremail@mail.domain'
+EMAIL_HOST_USER = 'youremail@mail.domain'
+EMAIL_HOST_PASSWORD = 'yourpass'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+PASSWORD_RESET_TIMEOUT = 3600
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

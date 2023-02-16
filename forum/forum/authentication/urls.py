f27c46a47d8b21a706887d6078ef786ec583c9db
Yehor Chervony <chervony.egor@gmail.com>
2023-02-16 14:35:41 +0200

    Added url to activate account by link from email

--- forum/forum/authentication/urls.py
+++ forum/forum/authentication/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('creatediscussion/', views.create_discussion, name="creatediscussion"),
     path('notifications/', views.notificationspage, name="notifications"),
     path('search/', views.search, name="search"),
+    path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('<str:page>/', views.themepage),
     path('<str:page>/<str:discussionid>/', views.discussionpage)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
